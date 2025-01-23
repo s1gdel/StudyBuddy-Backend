0474601251a58ff6be20d56185a90ee68d30a449
s1gdel <subhamsigdel@gmail.com>
2025-01-23 01:46:02 -0600

    Update ClassDataRepository.java

--- src/main/java/com/backend/backend/Repository/ClassDataRepository.java
+++ src/main/java/com/backend/backend/Repository/ClassDataRepository.java
@@ -1,7 +1,10 @@
 package com.backend.backend.Repository;
 
-import com.backend.backend.entity.ClassData; 
+import com.backend.backend.entity.ClassData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ClassDataRepository extends JpaRepository<ClassData, Long> {
+    List<ClassData> findByCreatedAtAfter(LocalDateTime createdAt);
 }
