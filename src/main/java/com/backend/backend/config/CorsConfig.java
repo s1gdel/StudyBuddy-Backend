e2deb07f31532abece10c17a1544293b1c505d20
s1gdel <subhamsigdel@gmail.com>
2025-01-25 23:06:06 -0600

    Update CorsConfig.java

--- src/main/java/com/backend/backend/config/CorsConfig.java
+++ src/main/java/com/backend/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000","https://utdsb.vercel.app") 
+                        .allowedOrigins("http://localhost:3000","https://sbcollege.vercel.app") 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
